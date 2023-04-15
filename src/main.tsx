0091f018ef1ce8efb716c3cec7dac3b080249020
jady-sm-godoi <jady.s.m@gmail.com>
2023-04-14 21:54:35 -0300

    rota 404

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import "./styles/global.css";
 import { Menu } from "./components/Menu";
 import { Post } from "./components/Post";
 import { Redirect } from "./components/Redirect";
+import { NotFound } from "./components/NotFound";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/posts" element={<Post />} />
         <Route path="/redirect" element={<Redirect />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
